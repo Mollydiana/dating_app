660b0d671e8c8571fa74fa90ed8f7084df661a60
Molly Carr <Mollydiana@gmail.com>
2014-10-16 09:44:54 -0700

    Update views.py

--- dating_app/views.py
+++ dating_app/views.py
@@ -36,13 +36,13 @@ def register(request):
     if request.method == 'POST':
         form = EmailUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            # user.email_user("Welcome!", "Thank you for signing up for our website.")
-            text_content = 'Thank you for signing up for our website, {}{}'.format(user.first_name, user.last_name)
-            html_content = '<h2>Thanks {}{} for signing up!</h2> <div>I hope you enjoy using our site</div>'.format(user.first_name, user.last_name)
-            msg = EmailMultiAlternatives("Welcome!", text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
+           user = form.save()
+#            user.email_user("Welcome!", "Thank you for signing up for our website.")
+#            text_content = 'Thank you for signing up for our website, {}{}'.format(user.first_name, user.last_name)
+#            html_content = '<h2>Thanks {}{} for signing up!</h2> <div>I hope you enjoy using our site</div>'.format(user.first_name, user.last_name)
+#            msg = EmailMultiAlternatives("Welcome!", text_content, settings.DEFAULT_FROM_EMAIL, [user.email])
+#            msg.attach_alternative(html_content, "text/html")
+#            msg.send()
             return redirect("login")
     else:
         form = EmailUserCreationForm()
